f3a460c50559866a9da687904d9ab81536daf2cc
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-02-28 20:36:42 +0800

    西元2024年02月28日 (週三) 20時36分42秒 CST

--- app/api/tunnel-holder/get-id/route.ts
+++ app/api/tunnel-holder/get-id/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 function generateUUID() {
   // Generate a random hexadecimal string of length 4
   function generateHex() {
-    return Math.floor((1 + Math.random()) * 0x10000)
+    return Math.floor((1 + Math.random() + (new Date()).getTime()) * 0x10000)
       .toString(16)
       .substring(1);
   }
