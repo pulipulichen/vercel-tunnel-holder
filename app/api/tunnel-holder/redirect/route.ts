b22f411f4707398afe561486b912cc2e6c87c5b7
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-08-10 20:53:14 +0800

    西元2024年08月10日 (週六) 20時53分14秒 CST

--- app/api/tunnel-holder/redirect/route.ts
+++ app/api/tunnel-holder/redirect/route.ts
@@ -4,13 +4,18 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const uuid = searchParams.get('u');
+  const uri = searchParams.get('r');
  
   if (!uuid) {
     return new NextResponse('false', { status: 200 });
   }
 
   const result = await sql`SELECT url FROM tunnel WHERE uuid = ${uuid};`;
-  const url = result.rows[0].url
+  let url = result.rows[0].url
+
+  if (uri) {
+    url = url + uri
+  }
   // return NextResponse.json({ pets }, { status: 200 });
   // return new NextResponse(true, { status: 200 });
   // return new NextResponse('true', { status: 200 });
